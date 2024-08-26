4e518db34e31d15e68f5cb5618d62f1c9808103c
Vinícius Lameirão <viniciuslameirao@hotmail.com>
2024-08-25 22:59:23 -0300

    removing some comments

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, redirect, render_template, request
 from werkzeug.utils import secure_filename
 
 #uploadPath = str(input("Where do you want you upload files? ")).replace("\\", "/") # where your upload files are going to
-# i've to work on it
+# I've to work on it
 
 app = Flask(__name__)
 
@@ -22,16 +22,14 @@ def upload():
             return redirect(request.url)
         files = request.files.getlist("file") 
         for file in files:
-            print(file.filename)
             fileName = secure_filename(file.filename)
             file.save(os.path.join(uploadPath, fileName))
-            print(os.path.join(uploadPath, fileName))
     uploadedFiles = os.listdir(uploadPath) # list of all files that already are in uploaded at current path
     
     for file in uploadedFiles: # filtering what is not a file 
         if "." not in file:
             uploadedFiles.remove(file)
-    print(uploadedFiles)
+    #print(uploadedFiles)
     return render_template("index.html", uploadPath=uploadPath, uploadedFiles=uploadedFiles)
 
 if __name__ == "__main__":
